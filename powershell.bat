32defa6fbd8b2cb102ccb2d0dd962e54dbf061bf
Yunus KONYAR <yunusknyr@hotmail.com>
2018-07-19 16:24:43 +0300

    Create powershell.bat

---
+++ powershell.bat
@@ -0,0 +1,87 @@
+Get-AppxPackage *Microsoft | volsnap.inf* | Remove-AppxPackage
+Get-AppxPackage *3DBuilder* | Remove-AppxPackage
+Get-AppxPackage *Accessibility tools* | Remove-AppxPackage
+Get-AppxPackage *Advertising.Xaml* | Remove-AppxPackage
+Get-AppxPackage *Auto-Rotation Manager* | Remove-AppxPackage
+Get-AppxPackage *AutoPlay* | Remove-AppxPackage
+Get-AppxPackage *BingWeather* | Remove-AppxPackage
+Get-AppxPackage *BioEnrollment* | Remove-AppxPackage
+Get-AppxPackage *Cache and temp files* | Remove-AppxPackage
+Get-AppxPackage *Contact Data* | Remove-AppxPackage
+Get-AppxPackage *ContactSupport* | Remove-AppxPackage
+Get-AppxPackage *ContentDeliveryManager* | Remove-AppxPackage
+Get-AppxPackage *Cortana* | Remove-AppxPackage
+Get-AppxPackage *CortanaListenUIApp* | Remove-AppxPackage
+Get-AppxPackage *DesktopAppInstaller* | Remove-AppxPackage
+Get-AppxPackage *DVD Play* | Remove-AppxPackage
+Get-AppxPackage *Ease Of Access Themes* | Remove-AppxPackage
+Get-AppxPackage *Kernel Debugging* | Remove-AppxPackage
+Get-AppxPackage *LockApp* | Remove-AppxPackage
+Get-AppxPackage *Manifest Backup* | Remove-AppxPackage
+Get-AppxPackage *Manual Setup* | Remove-AppxPackage
+Get-AppxPackage *Messaging* | Remove-AppxPackage
+Get-AppxPackage *Microsoft3DViewer* | Remove-AppxPackage
+Get-AppxPackage *MicrosoftEdge* | Remove-AppxPackage
+Get-AppxPackage *MicrosoftOfficeHub* | Remove-AppxPackage
+Get-AppxPackage *MicrosoftSolitaireCollection* | Remove-AppxPackage
+Get-AppxPackage *MicrosoftStickyNotes* | Remove-AppxPackage
+Get-AppxPackage *MiracastView* | Remove-AppxPackage
+Get-AppxPackage *NET.Native.Framework.1.3* | Remove-AppxPackage
+Get-AppxPackage *NET.Native.Runtime.1.3* | Remove-AppxPackage
+Get-AppxPackage *NET.Native.Runtime.1.4* | Remove-AppxPackage
+Get-AppxPackage *Office.OneNote* | Remove-AppxPackage
+Get-AppxPackage *On-Screen Keyboard* | Remove-AppxPackage
+Get-AppxPackage *OneConnect* | Remove-AppxPackage
+Get-AppxPackage *OneDrive* | Remove-AppxPackage
+Get-AppxPackage *Other Themes* | Remove-AppxPackage
+Get-AppxPackage *Paint 3D* | Remove-AppxPackage
+Get-AppxPackage *People* | Remove-AppxPackage
+Get-AppxPackage *Photos* | Remove-AppxPackage
+Get-AppxPackage *PlayReady* | Remove-AppxPackage
+Get-AppxPackage *Retail Demo Content* | Remove-AppxPackage
+Get-AppxPackage *Screensavers* | Remove-AppxPackage
+Get-AppxPackage *Setting Sync* | Remove-AppxPackage
+Get-AppxPackage *Shared PC mode* | Remove-AppxPackage
+Get-AppxPackage *Skype ORTC* | Remove-AppxPackage
+Get-AppxPackage *SkypeApp* | Remove-AppxPackage
+Get-AppxPackage *SmartScreen* | Remove-AppxPackage
+Get-AppxPackage *Sound Themes* | Remove-AppxPackage
+Get-AppxPackage *Sounds* | Remove-AppxPackage
+Get-AppxPackage *Speech TTS* | Remove-AppxPackage
+Get-AppxPackage *Steps Recorder* | Remove-AppxPackage
+Get-AppxPackage *StorePurchaseApp* | Remove-AppxPackage
+Get-AppxPackage *System Restore* | Remove-AppxPackage
+Get-AppxPackage *Text prediction* | Remove-AppxPackage
+Get-AppxPackage *Tips* | Remove-AppxPackage
+Get-AppxPackage *Unified Telemetry Client* | Remove-AppxPackage
+Get-AppxPackage *User Data Access* | Remove-AppxPackage
+Get-AppxPackage *User Data Storage* | Remove-AppxPackage
+Get-AppxPackage *VCLibs.140.00* | Remove-AppxPackage
+Get-AppxPackage *Volume Shadow Copy* | Remove-AppxPackage
+Get-AppxPackage *Wallet* | Remove-AppxPackage
+Get-AppxPackage *WindowPicker* | Remove-AppxPackage
+Get-AppxPackage *Windows Backup* | Remove-AppxPackage
+Get-AppxPackage *Block Level Backup Service Engine* | Remove-AppxPackage
+Get-AppxPackage *Windows Biometric Service* | Remove-AppxPackage
+Get-AppxPackage *Windows Disc Image Burning Tool* | Remove-AppxPackage
+Get-AppxPackage *Windows Error Reporting* | Remove-AppxPackage
+Get-AppxPackage *Windows Insider Hub* | Remove-AppxPackage
+Get-AppxPackage *Windows Mixed Reality* | Remove-AppxPackage
+Get-AppxPackage *Windows Recovery* | Remove-AppxPackage
+Get-AppxPackage *Windows System Assessment Tool* | Remove-AppxPackage
+Get-AppxPackage *WindowsAlarms* | Remove-AppxPackage
+Get-AppxPackage *WindowsCalculator* | Remove-AppxPackage
+Get-AppxPackage *WindowsCamera* | Remove-AppxPackage
+Get-AppxPackage *windowscommunicationsapps* | Remove-AppxPackage
+Get-AppxPackage *WindowsFeedbackHub* | Remove-AppxPackage
+Get-AppxPackage *WindowsMaps* | Remove-AppxPackage
+Get-AppxPackage *WindowsSoundRecorder* | Remove-AppxPackage
+Get-AppxPackage *WindowsStore* | Remove-AppxPackage
+Get-AppxPackage *Xbox App* | Remove-AppxPackage
+Get-AppxPackage *XboxGameCallableUI* | Remove-AppxPackage
+Get-AppxPackage *XboxGameOverlay* | Remove-AppxPackage
+Get-AppxPackage *XboxIdentityProvider* | Remove-AppxPackage
+Get-AppxPackage *XboxSpeechToTextOverlay* | Remove-AppxPackage
+Get-AppxPackage *ZuneMusic* | Remove-AppxPackage
+Get-AppxPackage *ZuneVideo* | Remove-AppxPackage
+Get-AppxPackage *Speech Recognition* | Remove-AppxPackage
